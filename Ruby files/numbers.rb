a9fd51500529081a062775d75cad0cd06f990852
Pedro Henrique <pedrro2@hotmail.com>
2015-09-29 14:01:47 +0000

    adding exercises of numbers

---
+++ Ruby files/numbers.rb
@@ -0,0 +1,8 @@
+puts "How many hours are in a year?"
+puts (365 * 24).to_s + " hours";
+
+puts "How many minutes are in a decade?"
+decade = 24 * 365;
+decade = decade * 10;
+decade = decade * 60;
+puts decade;
